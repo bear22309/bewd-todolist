348ed6cc0cc48a9167479f752180110db3d6aa33
Tony <anthonymostrowski@gmail.com>
2024-06-17 12:00:14 -0400

    Fix and cleanup TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,15 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-    render 'tasks/index' # can be omitted
+    render 'tasks/index' # Can be omitted if following Rails convention
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      render 'tasks/create' # can be omitted
+      render 'tasks/create' # Optional, if following convention, Rails renders 'create'
+    else
+      render json: { success: false, errors: @task.errors.full_messages }
     end
   end
 
@@ -23,21 +25,25 @@ class TasksController < ApplicationController
   def mark_complete
     @task = Task.find_by(id: params[:id])
     if @task && @task.update(completed: true)
-      render 'tasks/mark_complete'
+      render 'tasks/mark_complete' # Optional, depends on your rendering strategy
+    else
+      render json: { success: false }
     end
   end
 
   def mark_active
     @task = Task.find_by(id: params[:id])
     if @task && @task.update(completed: false)
-      render 'tasks/update'
+      render 'tasks/update' # Optional, based on how you want to render
+    else
+      render json: { success: false }
     end
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :completed)
   end
 end
 
